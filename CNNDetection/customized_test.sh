f6c5bcb434dc7563a62de60edc45b07bfacb8159
xrr-233 <ruixu33-c@my.cityu.edu.hk>
2022-11-28 22:26:29 +0800

    delete irrelevant files

--- CNNDetection/customized_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BATCH_SIZE=32 # default=64
-MODEL_PATH='./checkpoints/cs4487_proj/model_epoch_best.pth'
-DIR="../dataset/test"
-
-python demo_dir.py -j 0 -d $DIR -m $MODEL_PATH -b $BATCH_SIZE
-# python eval.py --model_path=$MODEL_PATH --dataroot=$DATA_ROOT
